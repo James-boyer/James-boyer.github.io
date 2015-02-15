51df266953a964b26557a3a76502179292fd5919
james <james.boyer4@gmail.com>
2015-02-15 16:10:06 -0500

    simpledrawer

---
+++ prjtest/js/simpledrawing.js
@@ -0,0 +1,52 @@
+ var stage, oX, oY, size, color, start, i;
+ var drawing = [];
+ var LeftArrow = 37;
+ var SHIFT = 16;
+ function keydown(event) {
+ //removes previous drawing
+   if(event.keyCode == LeftArrow){
+	   stage.removeChild(drawing[i - 1]);
+		 i--;
+	 }
+	 stage.update();
+  }
+  function init() {
+    stage = new createjs.Stage("demoCanvas");
+  	stage.enableDOMEvents(true);
+			
+		color = "#0FF";
+		size = 5;
+    start = false;
+		i = 0;
+			
+			// adding new shape to canvas
+		stage.on("stagemousedown",function(evt) {
+	    start = true;
+ 	    drawing[i] = new createjs.Shape();
+      stage.addChild(drawing[i]);
+		})
+			
+			// moving on
+		stage.on("stagemouseup",function(evt) {
+		   start = false;
+			 i++;
+i		})
+			
+		stage.on("stagemousemove",function(evt) {
+			 
+      if(oX && start){
+			  drawing[i].graphics.beginStroke(color)
+                            .setStrokeStyle(size,"round")
+                            .moveTo(oX,oY)
+                            .lineTo(evt.stageX,evt.stageY);
+				 
+        stage.update();	
+      }	
+      oX = evt.stageX;
+      oY = evt.stageY;				   
+i	  })
+            
+		this.document.onkeydown = keydown;
+
+    stage.update();
+    }
